bd83925b6963b922f1b46d48b50017284331fcd5
patriziorenelli <patriziore.pr@gmail.com>
2025-12-08 18:38:34 +0100

    Aggiunta funzione per scaricare la lista dei nomi dei file contenuti in Hug

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from datasets import load_dataset
-from huggingface_hub import list_repo_files, login, hf_hub_download
+from huggingface_hub import list_repo_files, login, hf_hub_download, HfApi
 import os
 import shutil
 
-login("mioTokenHug")
+login("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")
 repo_id = "PiantoDiGruppo/AMLDataset2"
 video_path_in_repo = "train/01-01-01-01-01-01-01.mp4"
 output_file = "D:/Users/Patrizio/Desktop/AISpeeh/AISpeechEmotionAnalyzer/video_completo.mp4"
@@ -23,3 +23,16 @@ def download_single_video_from_hug(repo_id, video_path_in_repo, output_file):
 
 
 download_single_video_from_hug(repo_id, video_path_in_repo, output_file)
+
+
+
+def get_file_list_names(repo_id):
+
+    api = HfApi()
+    # Ottieni la lista di tutti i file nel repo
+    files = api.list_repo_files(repo_id=repo_id, repo_type="dataset")
+    # Filtra quelli nella cartella train
+    train_files = [f for f in files if f.startswith("train/")]
+    return train_files
+
+print(get_file_list_names(repo_id))
