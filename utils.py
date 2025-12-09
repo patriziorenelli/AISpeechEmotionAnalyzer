e8f0c1a19353f7db99981291d9fdce5a6cc5c403
Mattpower11 <matteo.rocco.lt@gmail.com>
2025-12-09 21:38:54 +0100

    Added methods to utils

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import json
 import logging
-import random
 import sys
+import shutil
+
+
+from huggingface_hub import list_repo_files, hf_hub_download
 import evaluate
 import numpy as np
 from sklearn.calibration import LabelEncoder
@@ -170,3 +173,31 @@ class Utils:
             raise ValueError(f"Unknown model name: {model_name}")
 
         return model
+    
+    def download_single_video_from_hug(repo_id, video_path_in_repo, output_file):
+        # Scarica il file
+        local_path = hf_hub_download(
+            repo_id=repo_id,
+            filename=video_path_in_repo,
+            repo_type="dataset"
+        )
+        # Copia (o sposta) il file nella destinazione finale
+        shutil.copy(local_path, output_file)  # se vuoi rimuovere il file originale, usa shutil.move()
+
+        print(f"Video salvato come {output_file}")
+
+
+    def get_file_list_names(repo_id):
+        # Ottieni la lista di tutti i file nel repo
+        files = list_repo_files(repo_id=repo_id, repo_type="dataset")
+        # Filtra quelli nella cartella train
+        train_files = [f for f in files if f.startswith("train/")]
+        return train_files
+
+
+        # vediamo numero di frame con viso 
+        #  se n > soglia down sampling allora peschiamo gli x con prob più alta
+        #  se n = soglia li prendiamo tutti
+        #  se n < soglia li prendiamo tutti + frame neri? 
+        #  se n = 0 tutti frame neri ?    
+    
